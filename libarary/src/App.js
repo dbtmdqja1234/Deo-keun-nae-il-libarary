81727ca83545f3983713de4d67a16351a72d2827
your_name <ysb2497@naver.com>
2023-05-15 10:22:11 +0900

    리액트 생성
    리액트 생성.

---
+++ libarary/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Home from "./component/home";
+
+function App() {
+  return <Home></Home>;
+}
+
+export default App;
---
+++ libarary/src/component/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <nav>
+      <div>
+        <img src="http://www.jdnc.or.kr/img/common/logo.png"></img>
+      </div>
+      <div>
+        <img src="http://www.jdnc.or.kr/img/common/btn_gnbon.png"></img>
+      </div>
+    </nav>
+  );
+};
+
+export default Home;
